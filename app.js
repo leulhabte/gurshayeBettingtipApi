6ed59433e2e13496a3fb087b6f668f1c31266ae9
Leul_Hbt <leulh63@gmail.com>
2020-11-03 05:57:19 -0800

    V 1.0.0

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express');
 require('express-async-errors');
 const mongoose = require('mongoose');
-const user = require('./routes/user');
-const betting_tips = require('./routes/betting_tips');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-mongoose.connect(`mongodb+srv://leulhabte:${process.env.MongoSecret}@cluster0.saiuw.mongodb.net/gurshaye?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> console.log('connected...'))
-    .catch(err => {console.log(err.message)})
 
 const app = express();
 
-// Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/gurshaye', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(console.log('connected...'))
+    .catch(err => { console.log(err) })
 
-app.use('/api/user', user);
-app.use('/api/betting_tips', betting_tips);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use((error, req, res, next)=>{
-    res.status(500).json({error: error.message});
+const user = require('./routes/user');
+const betting_tips = require('./routes/betting_tips');
+
+app.use('/api/user/', user);
+app.use('/api/betting_tips/', betting_tips);
+
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        error
+    })
 })
 
 module.exports = app;
